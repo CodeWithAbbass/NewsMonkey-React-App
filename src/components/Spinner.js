cb180a57a4fb50a47a0fad2edf66fb151153cc9b
Abbas Ali <Abbas152052@gmail.com>
2022-10-18 23:14:20 +0500

    #

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Spinner } from ".../public/Spinner";
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Spinner
